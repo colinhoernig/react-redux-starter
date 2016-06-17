4a05cc19dafe3a46650fc700e8d5e8fc6d8417b2
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-06-17 17:18:38 -0500

    Add react-redux and Provider component.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import App from './components/App';
@@ -7,6 +8,8 @@ import App from './components/App';
 const store = configureStore();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
