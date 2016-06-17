50073ca91f4080ccb1495ffcef2faa6c60a0f057
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-06-17 15:39:07 -0500

    Add App component.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to Simple React Redux Starter.
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Simple React Redux starter');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
